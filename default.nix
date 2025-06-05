cedfb74cb070ac0e0dbea4d1ae4ad133c9c46c12
niccogc <nicci@dtu.dk>
2025-06-05 16:55:49 +0200

    a

--- default.nix
+++ default.nix
@@ -1,15 +1,15 @@
 {pkgs ? import <nixpkgs> {}}:
 pkgs.rustPlatform.buildRustPackage rec {
   pname = "hx-lsp";
-  version = "0.2.1";
+  version = "0.2.8";
   useFetchCargoVendor = true;
   src = pkgs.fetchFromGitHub {
     owner = "erasin";
     repo = "hx-lsp";
-    rev = "8cb9e1674fe2ada832ad5280f656b4513378a364";
-    # sha256 = pkgs.lib.fakeHash;
-    sha256 = "sha256-UFsOUPvH0Yw8kebjE0UZrizXlxcANqAKRvbQVcwSebo=";
+    rev = "b0fab6d543962aaf2b7e544c901a096b7a03948d";
+    sha256 = pkgs.lib.fakeHash;
+    # sha256 = "sha256-UFsOUPvH0Yw8kebjE0UZrizXlxcANqAKRvbQVcwSebo=";
   };
-  cargoHash = "sha256-72QMr7ZR/bd9AqATd65Ou/ngH9zQJc043AxltRzPV1M=";
-  # cargoHash = pkgs.lib.fakeHash;
+  # cargoHash = "sha256-72QMr7ZR/bd9AqATd65Ou/ngH9zQJc043AxltRzPV1M=";
+  cargoHash = pkgs.lib.fakeHash;
 }
